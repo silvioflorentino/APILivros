5a3657f6441f22bebab5fef7ae4f54dee73deafe
silvioflorentino <professorsilvioflorentino@yahoo.com.br>
2024-08-09 22:58:46 -0300

    controller

--- app/Http/Controllers/TbllivrosController.php
+++ app/Http/Controllers/TbllivrosController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\tbllivros;
+
 
 class TbllivrosController extends Controller
 {
@@ -12,18 +18,49 @@ class TbllivrosController extends Controller
     //Crud -> Read(leitura) Select/Visualizar
 
     public function index(){
+        $regBook = tbllivros::All();
+        $contador = $regBook->count();
 
+        return 'Livros: '.$contador.$regBook.Response()->json([],Response::HTTP_NO_CONTENT);
     }
-
     //Mostrar um tipo de registro especifico
     //Crud -> Read(leitura) Select/Visualizar
-    public function show(){
+    //A função show busca a id e retorna se o livros foram localizados por id.
 
+    public function show(string $id){ 
+        $regBook = tbllivros::find($id);
+
+        if($regBook){
+            return 'Livros Localizados: '.$regBook.Response()->json([],Response::HTTP_NO_CONTENT);
+        }else{
+            return 'Livros não localizados. '.Response()->json([],Response::HTTP_NO_CONTENT);
+        }
     }
 
     //Cadastrar registros
     //Crud -> Create(criar/cadastrar)
-    public function store(){
+    public function store(Request $request){
+        $regBook = $request->All();
+
+        $regVerifq = Validator::make($regBook,[
+            'nomeLivro'=>'required',
+            'generoLivro'=>'required',
+            'anoLivro'=>'required'
+        ]);
+
+        if($regVerifq->fails()){
+            return 'Registros Invalidos: '.Response()->json([],Response::HTTP_NO_CONTENT);
+
+        }
+        $regBookCad = tbllivros::create($regBook);
+
+        if( $regBookCad){
+            return 'Livros cadastrados: '.Response()->json([],Response::HTTP_NO_CONTENT);
+
+        }else{
+            return 'Livros não cadastrados: '.Response()->json([],Response::HTTP_NO_CONTENT);
+
+        }
 
     }
 
